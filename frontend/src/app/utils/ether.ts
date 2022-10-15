de7b4c5a72f6f24ab2a0ca00f055811e8086e50c
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-10-15 13:59:46 -0300

    fix(ether): issue when parsing fraction of ether

--- frontend/src/app/utils/ether.ts
+++ frontend/src/app/utils/ether.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from 'ethers';
 import { formatEther as internalFormat, parseEther } from 'ethers/lib/utils';
 
-export const parseEtherToBigNumber = (ether: number) => BigNumber.from(ether).mul(BigNumber.from(10).pow(18));
+export const parseEtherToBigNumber = (ether: number) => parseEther(ether.toString());
 export const formatEther = internalFormat;
